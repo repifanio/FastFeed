561cb5542ef9764da29ec5f391e98255610d06b3
repifanio <rafael.cepifanio@gmail.com>
2020-02-02 00:06:02 -0300

    Feito o projeto padrão do desafio 2

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '127.0.0.1',
+  username: 'postgres',
+  password: 'postgres',
+  database: '',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
