57f4c27f202a51df9dc20010e641bd05ac7b6362
repifanio <rafael.cepifanio@gmail.com>
2020-03-10 00:05:45 -0300

    Validação de hora para retirada

--- src/app/controller/deliveryStatus.js
+++ src/app/controller/deliveryStatus.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { isBefore, parseISO, startOfHour, parse } from 'date-fns';
 
 import Delivery from '../models/Delivery';
 import Courier from '../models/Courier';
@@ -6,6 +7,7 @@ import Mail from '../../lib/Mail';
 
 class DeliveryStatus {
   async startDelivery(req, res) {
+    // Inicio da validação das informações de entrada
     const schema = Yup.object().shape({
       start_date: Yup.date().required(),
     });
@@ -13,9 +15,28 @@ class DeliveryStatus {
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ erro: 'validation fails' });
     }
+    // Fim da validação das informações de entradas
+
+    // Inicio da validação da horas (se maior que 8 e menor que 18)
+    const { start_date } = req.body;
+
+    const startDateDay = new Date();
+    const endDateDay = new Date();
+
+    startDateDay.setHours(5);
+    endDateDay.setHours(15);
+
+    if (
+      isBefore(parseISO(start_date), startOfHour(startDateDay)) ||
+      isBefore(startOfHour(endDateDay), parseISO(start_date))
+    ) {
+      return res.status(400).json({ error: 'This hour is not valid.' });
+    }
+    // Fim da validação de horas
 
     const { id } = req.params;
 
+    // Inicio da validação se existe uma entrega com esse ID
     const delivery = await Delivery.findByPk(id, {
       include: [
         {
@@ -29,9 +50,12 @@ class DeliveryStatus {
     if (!delivery) {
       return res.status(400).json({ error: 'delivery not found with this ID' });
     }
+    // Fim da validação se existe esse ID
 
+    // Update da entrega
     const newDelivery = await delivery.update(req.body);
 
+    // Envio do email
     await Mail.sendMail({
       to: `${newDelivery.courier.name} <${newDelivery.courier.email}>`,
       subject: 'New Delivery for you',
